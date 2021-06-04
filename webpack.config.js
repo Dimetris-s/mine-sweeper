0679b801f66146d0157028c372d88cea69f62aa3
DESKTOP-INE9M8E\User <dim3triss@gmail.com>
2021-06-04 17:06:16 +0300

    initialize application

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path')
+
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+
+
+module.exports = { 
+    context: path.resolve(__dirname, 'src'),
+    entry: './index.js',
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'bundle.[hash].js'
+    },
+    devServer: {
+        port: 3000,
+        open: true
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: 'index.html'
+        }),
+        new CleanWebpackPlugin()
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.s[ac]ss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            },
+            {
+                test: /\.(png|svg|jpe?g|webp)$/,
+                use: ['file-loader']
+            }
+        ]
+    } 
+}
