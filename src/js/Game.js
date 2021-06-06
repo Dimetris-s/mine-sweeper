ab144bd088949becf6ad8ee87c78b71627038d12
DESKTOP-INE9M8E\User <dim3triss@gmail.com>
2021-06-06 16:47:10 +0300

    added difficults, remove arghs from class methods, added method for check winning state, now mines setting is going on first cell open by user

--- src/js/Game.js
+++ src/js/Game.js
@@ -4,45 +4,46 @@ import {
 
 
 export default class Game {
-    isGameOver = false
-    minesLeft = this.mines
+    static difficults = {
+        '1': {columns: 10, rows: 10, mines: 10},
+        '2': {columns: 16, rows: 16, mines: 40},
+        '3': {columns: 36, rows: 16, mines: 99}
+    }
+    
 
-    constructor(options) {
-        this.mines = options.mines
-        this.rows = options.rows
-        this.columns = options.columns
-        this.difficult = options.difficult
+    isGameOver = null
+    
+    constructor(difficult) {
+        this.difficult = typeof difficult === 'number' ? Game.difficults[difficult] : difficult
+        this.mines = this.difficult.mines
+        this.rows = this.difficult.rows
+        this.columns = this.difficult.columns
+        this.minesLeft = this.mines
 
-        this.playfield = this.createPlayfield(this.rows, this.columns)
-        this.setMines(this.mines, this.rows, this.columns)
-        this.fillPlayfield(this.playfield)
+        this.playfield = this.createPlayfield()
     }
 
-    initPlayfield() {
-        this.createPlayfield()
-        this.setMines()
-        this.fillPlayfield()
-    }
 
-    setMines(mines, rows, columns) {
-        for (let i = 0; i < mines; i++) {
-            let y = rnd(0, rows - 1)
-            let x = rnd(0, columns - 1)
-            while (this.playfield[y][x] === 9) {
-                y = rnd(0, rows - 1)
-                x = rnd(0, columns - 1)
+
+    setMines(px, py) {
+        for (let i = 0; i < this.mines; i++) {
+            let y = rnd(0, this.rows - 1)
+            let x = rnd(0, this.columns - 1)
+            while (this.playfield[y][x] === 9 || (px === x && py === y)) {
+                y = rnd(0, this.rows - 1)
+                x = rnd(0, this.columns - 1)
             }
             this.playfield[y][x] = 9
         }
     }
 
-    createPlayfield(rows, columns) {
+    createPlayfield() {
         const playfield = []
-        for (let i = 0; i < rows; i++) {
+        for (let i = 0; i < this.rows; i++) {
             playfield[i] = []
         }
         for (let i = 0; i < playfield.length; i++) {
-            for (let j = 0; j < columns; j++) {
+            for (let j = 0; j < this.columns; j++) {
                 playfield[i][j] = 0
             }
         }
@@ -61,28 +62,28 @@ export default class Game {
     getAvaliableCells(x, y, playfield) {
         const avaliableCells = []
         if(playfield[y - 1] !== undefined && playfield[y - 1][x - 1] !== undefined) avaliableCells.push({x: x - 1, y: y - 1 })
-        if(playfield[y - 1] !== undefined && playfield[y - 1][x] !== undefined) avaliableCells.push({x, y: y - 1})
+        if(playfield[y - 1] !== undefined && playfield[y - 1][x]     !== undefined) avaliableCells.push({x, y: y - 1})
         if(playfield[y - 1] !== undefined && playfield[y - 1][x + 1] !== undefined) avaliableCells.push({x: x + 1, y: y - 1 })
-        if(playfield[y] !== undefined && playfield[y][x - 1] !== undefined) avaliableCells.push({x: x - 1, y })
-        if(playfield[y] !== undefined && playfield[y][x + 1] !== undefined) avaliableCells.push({x: x + 1, y })
+        if(playfield[y]     !== undefined && playfield[y][x - 1]     !== undefined) avaliableCells.push({x: x - 1, y })
+        if(playfield[y]     !== undefined && playfield[y][x + 1]     !== undefined) avaliableCells.push({x: x + 1, y })
         if(playfield[y + 1] !== undefined && playfield[y + 1][x - 1] !== undefined) avaliableCells.push({x: x - 1, y:y + 1 })
-        if(playfield[y + 1] !== undefined && playfield[y + 1][x] !== undefined) avaliableCells.push({x , y: y + 1 })
+        if(playfield[y + 1] !== undefined && playfield[y + 1][x]     !== undefined) avaliableCells.push({x , y: y + 1 })
         if(playfield[y + 1] !== undefined && playfield[y + 1][x + 1] !== undefined) avaliableCells.push({x: x + 1, y: y + 1 })
         return avaliableCells
     }
 
-    fillPlayfield(playfield) {
-        for (let y = 0; y < playfield.length; y++) {
-            for (let x = 0; x < playfield[y].length; x++) {
-                if(playfield[y][x] !== 9) {
-                    playfield[y][x] = this.getMinesAround(x, y, playfield)
+    fillPlayfield() {
+        for (let y = 0; y < this.playfield.length; y++) {
+            for (let x = 0; x < this.playfield[y].length; x++) {
+                if(this.playfield[y][x] !== 9) {
+                    this.playfield[y][x] = this.getMinesAround(x, y, this.playfield)
                 }
             }
         }
     }
 
     openCell(y, x) {
-        if(typeof this.playfield[y][x] !== 'string')
+        if(typeof this.playfield[y][x] === 'string') return
         if(this.playfield[y][x] === 0) {
             this.getAvaliableCells(x, y, this.playfield).forEach(({x, y}) => {
                 setTimeout(() => {
@@ -90,13 +91,36 @@ export default class Game {
                 }, 0)
             })
         }
-        if(this.playfield[y][x] === 9) this.isGameOver = true
+        if(this.playfield[y][x] === 9) {
+            this.isGameOver = 'lose'
+            console.log('looser');
+        }
         this.playfield[y][x] = this.playfield[y][x].toString()
+
+        if(this.checkWinnning()) {
+            this.isGameOver = 'win'
+            console.log('winner');
+        }
+        
     }
+
+    checkWinnning() {
+        let win = true
+        for (let y = 0; y < this.playfield.length; y++) {
+            for (let x = 0; x < this.playfield[y].length; x++) {
+                if(typeof this.playfield[y][x] === 'number' && this.playfield[y][x] !== 9) {
+                    win = false
+                }
+            }
+        }
+        return win
+    }
+
     getState() {
         return {
-            isGameOver: this.isGameOver,
-            playfield: this.playfield
+            gameOver: this.isGameOver,
+            playfield: this.playfield,
+            mines: this.minesLeft
         }
     }
 }
